5e28dd77638179002d287789c2d5a720464f1c7f
s-tanoue <satoshi.tanoue@veriserve.co.jp>
2020-11-30 21:20:09 +0900

    正しく落ちるように修正

--- test/MessageComponent.spec.js
+++ test/MessageComponent.spec.js
@@ -1,12 +1,12 @@
 
 import { mount } from '@vue/test-utils'
-import MessageComponent from '@/components/Logo.vue'
+import MessageComponent from '@/components/MessageComponent.vue'
 
 describe('メッセージを表示するコンポーネントのテスト', () => {
   test('Hello Worldとメッセージを表示する', () => {
         const wrapper = mount(MessageComponent, {
             propsData: {
-            msg: 'Hello world'
+                msg: 'Hello world'
             }
         })
     expect(wrapper.text()).toContain('Hello world')
